21725215ff72d2010528dcc50e37aa9ce498494a
Edder <egaddini.dev@gmail.com>
2023-10-14 23:07:03 -0300

    ajuste no call requester dashboard

--- lib/app/modules/call_requester/screen/call_requester_dashboard_bidings.dart
+++ lib/app/modules/call_requester/screen/call_requester_dashboard_bidings.dart
@@ -1,10 +1,13 @@
+import 'package:chamados/app/modules/call/call_repository_impl.dart';
 import 'package:get/get.dart';
 
 import 'call_requester_dashboard_controller.dart';
 
 class CallRequesterDashboardBindings extends Bindings {
+
   @override
   void dependencies() {
-    Get.lazyPut(() => CallRequesterDashboardController());
+    Get.lazyPut(() => CallRequesterDashboardController(callRepo: Get.put<CallRepository>(CallRepository())));
   }
+
 }
--- lib/app/modules/call_requester/screen/call_requester_dashboard_controller.dart
+++ lib/app/modules/call_requester/screen/call_requester_dashboard_controller.dart
@@ -1,23 +1,27 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:chamados/app/data/models/user_info_model.dart';
+
 import '../../../data/models/call.dart';
-import '../../../data/providers/local_storage/local_storage.dart';
+import '../../../data/services/app_config/service.dart';
 import '../../../widgets/custom_data_table/custom_paginated_data_table2.controller.dart';
 import '../../call/call_repository_impl.dart';
 
 class CallRequesterDashboardController extends CustomPaginatedDataTable2Controller<Call> {
   
   bool sort = true;
-  late  CallRepository _callRepo ;
+
+  final CallRepository callRepo;
+  CallRequesterDashboardController({
+    required this.callRepo,
+  });
 
   @override
   void onInit() {
-    LocalStorageServices().getUser().then((value) => {
-          _callRepo.getCallListByEmail(value!.email!).then(
-                (value) => {
-                  data.value = value,
-                  isLoading.value = false,
-                },
-              ),
-        });
+    UserInfoModel.fromJson(AppConfigService().instance().userData()).email;
+      callRepo.getCallListByEmail(UserInfoModel.fromJson(AppConfigService().instance().userData()).email!).then((value) => {
+        data.value = value,
+        isLoading.value = false,
+      });
     super.onInit();
   }
 }
